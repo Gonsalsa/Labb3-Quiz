4dda7bc2505250ee3d19d63f8446cd68c41a004a
Gonsalsa <sebgon02@gmail.com>
2025-11-03 17:10:56 +0100

    Started on the backend of the QuizCreator

---
+++ Labb3-NET22/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Labb3_NET22.DataModels;
+
+namespace Labb3_NET22
+{
+    public class FileManager
+    {
+        public static string Folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SavedQuizes");
+
+        public static async Task SaveQuiz(Quiz quiz)
+        {
+            Directory.CreateDirectory(Folder);
+            string title = string.Join("_", quiz.Title.Split(Path.GetInvalidFileNameChars()));
+            var path = Path.Combine(Folder, title + ".json");
+            var json = JsonSerializer.Serialize(quiz, new JsonSerializerOptions { WriteIndented = true });
+            await File.WriteAllTextAsync(path, json);
+        }
+
+    }
+}
---
+++ Labb3-NET22/QuizCreator.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Labb3_NET22.DataModels;
+
+namespace Labb3_NET22
+{
+    /// <summary>
+    /// Interaction logic for QuizCreator.xaml
+    /// </summary>
+    public partial class QuizCreator : UserControl
+    {
+        private Quiz CurrentQuiz = new Quiz();
+
+        public QuizCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddQuestionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveQuizButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
